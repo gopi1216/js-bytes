ce770794c0ad15123854f4825a421467ec38b837
J, Gopi <gopi.j2@unisys.com>
2021-11-09 18:02:52 +0530

    - New files updated.

---
+++ scripts/variables.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/* || Task 1
+-- Declare a variable called myName.
+-- Initialize myName variable with suitable value, on separate line.
+-- Declare a variable called myAge and initialize with a value, on same line.
+*/
+
+let myName,
+    myAge = 30;
+
+myName = 'Gopi';
+
+
+/* || Task 2
+-- Add new line to change the value in below variable.
+
+  var myName = 'Paul';
+*/
+myName = 'Krishna';
+
+
+/* || Task 3
+-- We have an existing code, which has errors present in it.
+-- Update the code fix the errors.
+
+  const userName = 'Default';
+  userName = 'Chris';
+
+  let userAge = 42;
+*/
+
+let userName = 'Default'; //we can't re initialize const variables.
