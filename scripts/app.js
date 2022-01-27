9066ba0b077b84ba9deed7295ec47156e086176c
J, Gopi <gopi.j2@unisys.com>
2022-01-27 22:06:30 +0530

    - Build your own function.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,43 @@
-var utils = (function () {
+function displayMessage(msgText, msgType) {
+  const main = document.querySelector('main'),
+    container = document.createElement('div'),
+    msg = document.createElement('p'),
+    closeButton = document.createElement('button');
 
-  function docReady(fn) {
-    if (document.readyState === 'complete' || document.readyState === 'interactive') {
-      setTimeout(fn, 1);
-    }
+  container.setAttribute('class', 'msgBox');
+  main.appendChild(container);
+
+  msg.textContent = msgText;
+  container.appendChild(msg);
+
+  switch (msgType) {
+    case 'info':
+      msg.style.background = 'dodger blue';
+      break;
+    case 'warning':
+      msg.style.background = 'yellow';
+      break;
+    case 'error':
+      msg.style.background = 'red';
+      break;
+    case 'success':
+      msg.style.background = '#035956';
+      break;
+    default:
+      msg.style.background = '#999';
   }
 
-  return {
-    docReady
-  };
+  closeButton.textContent = 'X';
+  closeButton.setAttribute('type', 'button');
+  container.appendChild(closeButton);
 
-})();
+  closeButton.addEventListener('click', () => {
+    container.parentNode.removeChild(container);
+  });
+}
+
+let addMessageBox = document.querySelector('.btn-secondary');
+
+addMessageBox.addEventListener('click', () =>
+  displayMessage("Yep, it's dynamic one..!", 'error')
+);
--- scripts/functions.js
+++ scripts/functions.js
@@ -8,9 +8,47 @@
 
 let names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada'];
 
-function chooseName () {
+function random() {
   let randomNumber = Math.floor(Math.random() * names.length);
+  chooseName(randomNumber);
+}
+
+function chooseName(randomNumber) {
   console.log(names[randomNumber]);
 }
 
-chooseName();
+random();
+
+/* || Task 2
+-- For our second functions-related task, you need to create a function that draws a rectangle on the provided <canvas> (reference variable canvas, context available in ctx), based on the five provided input variables:
+
+* x — the x coordinate of the rectangle.
+* y — the y coordinate of the rectangle.
+* width — the width of the rectangle.
+* height — the height of the rectangle.
+* color — the color of the rectangle.
+
+-- You'll want to clear the canvas before drawing, so that when the code is updated in the case of the live version, you don't get lots of rectangles drawn on top of one another.
+
+* const canvas = document.querySelector('canvas');
+* const ctx = canvas.getContext('2d');
+
+* const x = 50;
+* const y = 60;
+* const width = 100;
+* const height = 75;
+* const color = 'blue';
+*/
+const x = 50,
+  y = 60,
+  width = 100,
+  height = 75,
+  color = 'blue',
+  canvas = document.querySelector('canvas'),
+  ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.lineWidth = '4px';
+ctx.rect(x, y, width, height);
+ctx.strokeStyle = color;
+ctx.stroke();
