235015bbae019bb85d0c2d94a59896d9c86794e6
J, Gopi <gopi.j2@unisys.com>
2022-02-08 22:49:14 +0530

    - alt-iframe for structure purpose. - changes to actual structure and CSS also.

--- scripts/app.js
+++ scripts/app.js
@@ -35,9 +35,29 @@ function displayMessage(msgText, msgType) {
     container.parentNode.removeChild(container);
   });
 }
-
+/*
 let addMessageBox = document.querySelector('.btn-secondary');
 
 addMessageBox.addEventListener('click', () =>
   displayMessage("Yep, it's dynamic one..!", 'error')
 );
+
+function random(num) {
+  return Math.floor(Math.random() * num);
+}
+
+function changeBackground() {
+  let randomColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`,
+    heading = document.querySelector('.h2');
+
+  heading.style.backgroundColor = randomColor;
+}
+
+let changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', changeBackground, { once: true });
+*/
+function setTitle(title) {
+  let _title = document.querySelector('[data-title]');
+  _title.textContent = title;
+}
