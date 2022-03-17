bb53ba6cc5c4c8041dba7158b9c0974f2e3a3099
J, Gopi <gopi.j2@unisys.com>
2022-03-17 19:10:06 +0530

    - Calculate Area and Perimeter.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
-/**
+/*
 || Write a program to display current day and time in the following format.
--- Today is: Tuesday
--- Current Time is: 10 PM : 30 : 38
+? Today is: Tuesday.
+? Current Time is: 10 PM : 30 : 38
 */
 const date = new Date(),
   weekDays = [
@@ -22,9 +22,9 @@ let hours =
   minutes = date.getMinutes(),
   seconds = date.getSeconds();
 
-weekDays.forEach((day, index) => {
+weekDays.forEach((weekDay, index) => {
   if (index === date.getDay()) {
-    console.log(`Today is: ${day}`);
+    console.log(`Today is: ${weekDay}`);
   }
 });
 
@@ -32,6 +32,35 @@ console.log(`Current Time is: ${hours} : ${minutes} : ${seconds}`);
 
 /*
 || Write a program to print the contents of the current window.
+? window.print();
 */
 
-window.print()
+/*
+|| Write a program to get the current date.
+? Current Date: mm-dd-yyyy || mm/dd/yyyy || dd-mm-yyyy || dd/mm/yyyy
+*/
+let day = date.getDate(),
+  month = date.getMonth(),
+  year = date.getFullYear();
+
+day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+month = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+
+console.log(`Date is: ${day} - ${month} - ${year} `);
+console.log(`Date is: ${day} / ${month} / ${year} `);
+console.log(`Date is: ${month} - ${day} - ${year} `);
+console.log(`Date is: ${month} / ${day} / ${year} `);
+
+/*
+|| Write a program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7
+? Formula to calculate perimeter: (side1 + side2 + side3) / 3
+? Formula to calculate area: Math.sqrt(perimeter * (perimeter - side1) * (perimeter - side2) * (perimeter - side3))
+*/
+let calcArea = (side1, side2, side3) => {
+  let perimeter = (side1 + side2 + side3) / 2,
+    area = Math.sqrt(perimeter * ((perimeter - side1) * (perimeter - side2) * (perimeter - side3)))
+
+  console.log(`Ares is: ${ area }`)
+}
+
+calcArea(5, 6, 7)
