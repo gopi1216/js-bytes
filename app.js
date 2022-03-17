55b2618407a2c657126b10454431d610f7da711f
J, Gopi <gopi.j2@unisys.com>
2022-03-17 17:01:42 +0530

    -- W3 REsource 1

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-/* || Variables ================ */
-let contry = 'India',
-  continent = 'Asia',
-  population = '1.3b';
+'use strict';
+/**
+|| Write a program to display current day and time in the following format.
+-- Today is: Tuesday
+-- Current Time is: 10 PM : 30 : 38
+*/
+const date = new Date(),
+  weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-console.log(contry, continent, population);
+let hours =
+    date.getHours() >= 12
+      ? `0${date.getHours() - 12} PM`
+      : `${date.getHours()} AM`,
+  minutes = date.getMinutes(),
+  seconds = date.getSeconds();
+
+weekDays.forEach((day, index) => {
+  if (index === date.getDay()) {
+    console.log(`Today is: ${day}`);
+  }
+});
+
+console.log(`Current Time is: ${hours} : ${minutes} : ${seconds}`);
