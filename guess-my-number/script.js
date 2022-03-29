144d5f68443535108ae6e3210dbb2fef0dcabbaa
J, Gopi <gopi.j2@unisys.com>
2022-03-30 00:41:00 +0530

    - Final changes with Code refactoring.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,32 +1,56 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1,
+  score = 20,
+  highScore = 0;
+
+const printMessage = (message) => {
+  let $message = document.querySelector('.message');
+  $message.textContent = message;
+};
+
+const updateNumber = (number, size) => {
+  let $number = document.querySelector('.number');
+  $number.textContent = number;
+  $number.style.width = size;
+};
+
+const updateScore = (number) => {
+  let $score = document.querySelector('.score');
+  $score.textContent = number;
+};
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  let message = document.querySelector('.message');
 
   if (!guess) {
-    message.textContent = '⛔ No Number';
+    printMessage('⛔ No Number');
   } else if (guess === secretNumber) {
-    message.textContent = '🎉 Correct Number.';
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      message.textContent = '📉 Too Low.';
-      document.querySelector('.score').textContent = --score;
-    } else {
-      message.textContent = '👎 You Lost the Game';
-      document.querySelector('.score').textContent = 0;
+    printMessage('🎉 Correct Number.');
+    updateNumber(secretNumber, '30rem');
+    document.body.style.backgroundColor = '#04AA6D';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      message.textContent = '📈 Too high.';
-      document.querySelector('.score').textContent = --score;
+      printMessage(guess < secretNumber ? '📉 Too Low.' : '📈 Too high.');
+      updateScore(--score);
     } else {
-      message.textContent = '👎 You Lost the Game';
-      document.querySelector('.score').textContent = 0;
+      printMessage('👎 You Lost the Game');
+      updateScore(0);
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  printMessage('Start guessing...');
+  updateNumber('?', '15rem');
+  updateScore(score);
+  document.querySelector('.guess').value = '';
+  document.body.style.backgroundColor = '#222';
 });
